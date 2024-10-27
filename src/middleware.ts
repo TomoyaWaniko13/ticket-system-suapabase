59f8804b47cfc73b85a3651ec1afb34944e37ef6
tomoya <alligatorfree12@gmail.com>
2024-10-27 09:44:20 +0900

    Protecting access to the Ticket Management system

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,26 @@
 import { getSupabaseReqResClient } from '@/supabase-utils/reqResClient';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  const { supabase, response } = getSupabaseReqResClient({ request });
-  await supabase.auth.getSession();
+// P.101 Protecting access to the Ticket Management system
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const { supabase, response } = getSupabaseReqResClient({ request: req });
+
+  const session = await supabase.auth.getSession();
+  const requestedPath = req.nextUrl.pathname;
+  const sessionUser = session.data?.session?.user;
+
+  if (requestedPath.startsWith('/tickets')) {
+    if (!sessionUser) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  } else if (requestedPath === '/') {
+    if (sessionUser) {
+      return NextResponse.redirect(new URL('/tickets', req.url));
+    }
+  }
+
   return response.value;
 }
 
